87d67a0112261849cf882b445a35078b636ab55f
firmy <firmy@foxmail.com>
2013-03-07 16:38:24 +0800

    add first site named aaa

---
+++ caches/caches_tpl_data/caches_data/bda1e6fd4f709a2716e646a95b2b4f5d.cache.php
@@ -0,0 +1,30 @@
+<?php
+return array (
+  1 => 
+  array (
+    'id' => '1',
+    'catid' => '9',
+    'typeid' => '0',
+    'title' => 'ada',
+    'style' => '',
+    'thumb' => '',
+    'keywords' => 'ad',
+    'description' => 'adfad',
+    'posids' => '0',
+    'url' => 'http://phpcms.firmy.com/install_package/index.php?m=content&c=index&a=show&catid=9&id=1',
+    'listorder' => '0',
+    'status' => '99',
+    'sysadd' => '1',
+    'islink' => '0',
+    'username' => 'root',
+    'inputtime' => '1362643227',
+    'updatetime' => '1362643239',
+    'hitsid' => 'c-12-1',
+    'views' => '0',
+    'yesterdayviews' => '0',
+    'dayviews' => '0',
+    'weekviews' => '0',
+    'monthviews' => '0',
+  ),
+);
+?>
---
+++ caches/error_log.php
@@ -0,0 +1,14 @@
+<?php exit;?>03-07 14:39:18 | 2 | Invalid argument supplied for foreach() | phpcms/modules/content/classes/content_tag.class.php | 207
+<?php exit;?>03-07 14:39:18 | 2 | Invalid argument supplied for foreach() | phpcms/libs/functions/global.func.php | 1312
+<?php exit;?>03-07 14:39:18 | 2 | Invalid argument supplied for foreach() | phpcms/modules/content/classes/content_tag.class.php | 207
+<?php exit;?>03-07 14:39:23 | 2 | Invalid argument supplied for foreach() | phpcms/modules/content/classes/content_tag.class.php | 207
+<?php exit;?>03-07 14:39:23 | 2 | Invalid argument supplied for foreach() | phpcms/libs/functions/global.func.php | 1312
+<?php exit;?>03-07 14:39:23 | 2 | Invalid argument supplied for foreach() | phpcms/modules/content/classes/content_tag.class.php | 207
+<?php exit;?>03-07 14:41:44 | 2 | Invalid argument supplied for foreach() | phpcms/modules/content/create_html.php | 428
+<?php exit;?>03-07 14:42:07 | 2 | Invalid argument supplied for foreach() | phpcms/modules/content/classes/content_tag.class.php | 207
+<?php exit;?>03-07 14:42:07 | 2 | Invalid argument supplied for foreach() | phpcms/libs/functions/global.func.php | 1312
+<?php exit;?>03-07 14:42:07 | 2 | Invalid argument supplied for foreach() | phpcms/modules/content/classes/content_tag.class.php | 207
+<?php exit;?>03-07 14:44:07 | 2 | Invalid argument supplied for foreach() | phpcms/modules/content/classes/content_tag.class.php | 207
+<?php exit;?>03-07 14:44:07 | 2 | Invalid argument supplied for foreach() | phpcms/libs/functions/global.func.php | 1312
+<?php exit;?>03-07 14:44:07 | 2 | Invalid argument supplied for foreach() | phpcms/modules/content/classes/content_tag.class.php | 207
+<?php exit;?>03-07 14:44:13 | 2 | Invalid argument supplied for foreach() | phpcms/modules/content/create_html.php | 111
